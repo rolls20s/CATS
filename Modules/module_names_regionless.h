a1b5768808696f07b05c45bda0a4b63d58de3025
Falcon Street <rollstwenties@gmail.com>
2012-04-15 15:20:06 -0400

    Code::Blocks update

--- Modules/module_names_regionless.h
+++ Modules/module_names_regionless.h
@@ -33,49 +33,48 @@ Scans for valid names
 **********************************************************************************************/
 int module_names_regionless::scan( string &curr_line, std::vector<replacement> &name_repls )
 {
-    // String Iterators
-    std::string::const_iterator start = curr_line.begin();
-    std::string::const_iterator end = curr_line.end();
 
-    // Flags for regex_search()
-    boost::match_flag_type flags = boost::match_default;
+    /******************** Regular Expressions for regex_search() ********************/
+    boost::regex re_first(
+                            // First name
+                            "(first\\s*name:?\\s?(is)?\\s?([a-z\\']*))"
 
-    // Iterators for regex_search()
-    boost::match_results<std::string::const_iterator> match_first;  // Firstname matches
-    boost::match_results<std::string::const_iterator> match_last;   // Lastname matches
+                            "|" // OR
 
-    // Regular Expressions for regex_search()
-    boost::regex re_first( "first\\s*name:?\\s?(is)?\\s?([a-z\\']*)", boost::regex::icase );                // Firstname
-    boost::regex re_last( "((last\\s*name)|(surname)):?\\s?(is)?\\s?([a-z\\'\\-]*)", boost::regex::icase ); // Surname
+                            // Last name
+                            "(((last\\s*name)|(surname)):?\\s?(is)?\\s?([a-z\\'\\-]*))"
+
+                            // Not case sensitive
+                            , boost::regex::icase
+    );
+    /*********************************************************************************/
 
 
-    /****** FIRST NAME *********************************************************************/
-    if( boost::regex_search( start, end, match_first, re_first, flags ) )
-    {
-        repl_name.begin_pos = match_first[2].first - start; // Start position in line
-        repl_name.end_pos = match_first[2].second - start;  // End position in line
+    // Iterative search
+    boost::sregex_iterator it( curr_line.begin(), curr_line.end(), re_first );
+    boost::sregex_iterator end;
+
+
+    /****** Replace Names *********************************************************************/
+    while( it != end )
+    {/*
+        repl_name.begin_pos = match_first[3].first - start; // Start position in line
+        repl_name.end_pos = match_first[3].second - start;  // End position in line
         repl_name.value = "test1";                          // New value
 
         name_repls.push_back( repl_name ); // Add to the list of replacements in this line
-
-        //cout << match_first[2] << endl;
-    }
-    /***************************************************************************************/
-
-
-    /****** LAST NAME **********************************************************************/
-    if( boost::regex_search( start, end, match_last, re_last, flags ) )
+    */
+    /*int j = 0;
+    while(j<15)
     {
-        repl_name.begin_pos = match_last[5].first - start;  // Start
-        repl_name.end_pos = match_last[5].second - start;   // End
-        repl_name.value = "test2";                          // New value
-
-        name_repls.push_back( repl_name ); // Add to the list of replacements in this line
-
-        //cout << match_last[5] << endl;
+        cout << j << ' ' <<(*it)[j] << '\n';
+        j++;
+    }*/
+        cout << "Firstname: " << (*it)[3] << endl;
+        cout << "Last name: " << (*it)[9] << endl;
+        it++;
     }
     /***************************************************************************************/
-
 
     return OK;
 }
